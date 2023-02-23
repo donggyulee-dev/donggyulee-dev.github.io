18879dc4ddfc038e922a5b4738be264df29a5caa
donggyu lee <donggyulee.dev@gmail.com>
2023-02-23 10:23:40 +0900

    add count nums

--- main.js
+++ main.js
@@ -5,12 +5,15 @@ document.getElementById('first').innerHTML = 'Hello World';
 document.getElementById('count').innerHTML = 0;
 document.getElementById('up').onclick = function () {
     document.getElementById('count').innerHTML++;
+    console.log(document.getElementById('count').innerHTML);
 }
 document.getElementById('down').onclick = function () {
     document.getElementById('count').innerHTML--;
+    console.log(document.getElementById('count').innerHTML);
 }
 
 // show count in console when count change
 document.getElementById('count').onchange = function () {
     console.log(document.getElementById('count').innerHTML);
 }
+
